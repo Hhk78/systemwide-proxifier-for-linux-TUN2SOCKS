a293e199b0463588c65403d15ef6dcee873f218f
Fedor or Not <84645312+Hhk78@users.noreply.github.com>
2024-02-05 18:13:39 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# Feragatnâme
+Bu badvpn programının tun2socks eklentisini kullanarak sistem geneli bir proxy bağlantısı yapmaya yarayan yardımcı bir betiktir. Kullanılan herhangi bir yazılım bana ait değildir sadece yardım etmeye yarayan bir betiktir
+
+# Badvpn yazılımı resmi deposu (herkese açık olarak arşivlendi)
+https://github.com/ambrop72/badvpn
+Hehangi bir şekilde kaldırılırsa diye kendi hesabıma forkladım. Oradan da kullanabilirsiniz.
+
+# Derleme
+Sisteminize gereken derleme bağımlılıklarını kurun (örnek make, cmake ve gcc gibi)
+
+Sonra depoyu klonlayın;
+```bash
+git clone https://github.com/ambrop72/badvpn```
+
+Sonrasında ise şu komutları çalıştırın;
+```bash
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=.
+make -j4```
+
+Bu komutlardan sonra badvpn deposunu kolnaldığınız badvpn dizinin içindeki build/ klasösürünün içindeki tun2socks/badvpn-tun2socks çalıştırılabilir dosyasını /bin klasörüne kopyalayın.
+
+# Kullanım
+
+```bash
+sudo sh tun2socks.sh```
+
+Komutu ile kullanabilirsiniz.
---
+++ tun2socks.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# 5 saniyelik geri sayım
+for i in {5..1}; do
+    echo "Tun2socksu README.md dosyasında yazdığı gibi derleyip PATH değişkeninin olduğu herhangi bir dizinin (örnek: /bin)  içine badvpn-tun2socks olarak kopyalayıp çalıştırma yetkileri verin. Geri sayım: $i"
+    sleep 1
+done
+
+# Kullanıcıdan gerekli bilgileri al
+read -p "Proxy server IP adresini girin (v2ray veya shadowsocks gibi programlarda 127.0.0.1 yerine shadowsocksun bağlandığı sunucunun public IP adresini girin) : " proxy_server_ip
+read -p "İnternet çıkış arayüz adını girin (örneğin eth0): " internet_interface
+read -p "Proxy sunucu ip adresini ve portunu girin (eğer 127.0.01 kullanan v2ray gibi bir program var ise 127.0.0.1:port olarak girebilirsiniz): " real_proxy
+# Komutları çalıştır
+sudo badvpn-tun2socks --netif-ipaddr 10.0.0.1 --netif-netmask 255.255.255.0 --socks-server-addr $real_proxy --tundev tun0 --loglevel 1 &
+
+# Proxy bağlantısı için tun2socks'tan IP'yi hariç tut
+sudo ip route add $proxy_server_ip/32 via 192.168.1.1 dev $internet_interface
+
+# IP adresi atama
+sudo ip addr add 10.0.0.1/24 dev tun0
+sudo ip link set dev tun0 up
+
+# Default gateway'i (tun2socks) oluştur
+sudo ip route del default 2> /dev/null
+sudo ip route add default dev tun0 2> /dev/null
+echo "İşlem tamamlandı."
