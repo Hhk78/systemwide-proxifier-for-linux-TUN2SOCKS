0d343e9f833f4d5250a0047f02522a21c9a1adb9
Fedor or Not <84645312+Hhk78@users.noreply.github.com>
2024-02-08 14:58:16 +0300

    Update tun2socks.sh

--- tun2socks.sh
+++ tun2socks.sh
@@ -10,11 +10,14 @@ done
 read -p "Proxy server IP adresini girin (v2ray veya shadowsocks gibi programlarda 127.0.0.1 yerine shadowsocksun bağlandığı sunucunun public IP adresini girin) : " proxy_server_ip
 read -p "İnternet çıkış arayüz adını girin (örneğin eth0): " internet_interface
 read -p "Proxy sunucu ip adresini ve portunu girin (eğer 127.0.01 kullanan v2ray gibi bir program var ise 127.0.0.1:port olarak girebilirsiniz): " real_proxy
+sudo ip route | grep '^default' | cut -d ' ' -f 2-
+
+read -p "İnternete bağlı olduğunuz arayüzün varsayılan ağ geçidi: " varsayilan_interface
 # Komutları çalıştır
 sudo badvpn-tun2socks --netif-ipaddr 10.0.0.1 --netif-netmask 255.255.255.0 --socks-server-addr $real_proxy --tundev tun0 --loglevel 1 &
 
 # Proxy bağlantısı için tun2socks'tan IP'yi hariç tut
-sudo ip route add $proxy_server_ip/32 via 192.168.1.1 dev $internet_interface
+sudo ip route add $proxy_server_ip/32 via $varsayilan_interface dev $internet_interface
 
 # IP adresi atama
 sudo ip addr add 10.0.0.1/24 dev tun0
